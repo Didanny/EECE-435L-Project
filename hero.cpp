e8013f474f9299382d1cbf644ffac0c831689d0f
Danny Abraham <daa50@mail.aub.edu>
2018-11-03 18:32:37 +0200

    Create move function for hero

--- hero.cpp
+++ hero.cpp
@@ -13,7 +13,43 @@ void Hero::setDirection(Direction direction)
 }
 
 #include <QDebug>
+#include <QThread>
+#include <QMutex>
+#include <QWaitCondition>
+#include <QApplication>
 void Hero::move(int x)
 {
+    int step = 45;
+    if (x < 0)
+    {
+        step = -1 * step;
+        x = -1 * x;
+    }
     qDebug() << "Inside hero.move";
+    for (int i = x; i > 0; i--)
+    {
+        switch(_direction)
+        {
+        case RIGHT:
+            this->setPos(this->x() + step, this->y());
+            break;
+        case UP:
+            this->setPos(this->x(), this->y() - step);
+            break;
+        case LEFT:
+            this->setPos(this->x() - step, this->y());
+            break;
+        case DOWN:
+            this->setPos(this->x(), this->y() + step);
+            break;
+        default:
+            break;
+        }
+
+        QMutex localMutex;
+        QWaitCondition sleepSimulator;
+        sleepSimulator.wait(&localMutex, 200);
+        QApplication::processEvents();
+
+    }
 }
